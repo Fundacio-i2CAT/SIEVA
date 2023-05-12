47db17848722e30f7eec2e1dde13ee64355f7a78
Ortiz, Nil <nil.ortiz@i2cat.net>
2023-05-12 15:25:48 +0200

    Update main.py

--- AI_Engine/main.py
+++ AI_Engine/main.py
@@ -46,7 +46,7 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
